0938b6f851aaec834d4473787d8834ed1e274d92
William Keith Pendleton <wkp23@byu.edu>
2025-05-27 08:00:24 -0600

    Chapter 2: A New Application

---
+++ estate/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Estate Management",
+    "version": "1.0",
+    "summary": "Manage properties, clients, and sales",
+    "depends": ["base"],
+    "data": [
+    ],
+    "application": True,
+    "installable": True,
+    "license": "LGPL-3",
+}
